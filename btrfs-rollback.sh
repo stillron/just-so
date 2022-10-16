60d982f04cd250c15b7e65dfe94c1eeda709084b
Ron Houk <ron@debian-BULLSEYE-live-builder-AMD64>
2022-10-16 13:51:42 -0400

    removed absolute paths

--- btrfs-rollback.sh
+++ btrfs-rollback.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
+#Variables
+
+# if [[ -n $(which btrfs) ]]; then
+    # BTRFS=$(which btrfs)
+# fi
+
 #Functions
 
 rollback() {
-    /usr/bin/mount /dev/vda3 /root/topsubs
-
-    LATEST=$(stat -c '%y %n' /root/topsubs/@snapshots/$1/* | sort -n -r | head -n 1 | cut -f 4 -d " ")
-    echo $LATEST
-
-    /usr/bin/umount /home/"$1"
-
+    mount /dev/vda3 /root/topsubs
+    LATEST=$(stat -c '%y %n' /root/topsubs/@snapshots/"$1"/* | sort -n -r | head -n 1 | cut -f 4 -d " ")
+    # echo "$LATEST"
+    umount /home/"$1"
     echo "@$1"
-    /usr/bin/btrfs subvolume delete -c /root/topsubs/@$1
-
-    /usr/bin/btrfs subvolume snapshot $LATEST /root/topsubs/@$1
-
-    /usr/bin/umount /dev/vda3
-
-    /usr/bin/mount /home/"$1"
+    btrfs subvolume delete -c /root/topsubs/@"$1"
+    btrfs subvolume snapshot "$LATEST" /root/topsubs/@"$1"
+    umount /dev/vda3
+    mount /home/"$1"
 }
 
 #Determine which function the user passed in to the command
 
 case "$1" in
-    "") ;;
-    rollback) "$@"; exit;;
-    *) echo "Unknown function: $1"; exit 2;;
+"") ;;
+rollback)
+    "$@"
+    exit
+    ;;
+*)
+    echo "Unknown function: $1"
+    exit 2
+    ;;
 esac
