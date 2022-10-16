31d6d05e32997c740768197ddb9d9444d6302429
Ron Houk <ron@debian-BULLSEYE-live-builder-AMD64>
2022-10-16 12:46:41 -0400

    started adding functions

--- btrfs-rollback.sh
+++ btrfs-rollback.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-/usr/bin/mount /dev/vda3 /root/topsubs
+#Functions
 
-LATEST=$(stat -c '%y %n' /root/topsubs/@snapshots/* | sort -n -r | head -n 1 | cut -f 4 -d " ")
-echo $LATEST
+rollback() {
+    /usr/bin/mount /dev/vda3 /root/topsubs
 
-/usr/bin/umount /home/"$1"
+    LATEST=$(stat -c '%y %n' /root/topsubs/@snapshots/$1/* | sort -n -r | head -n 1 | cut -f 4 -d " ")
+    echo $LATEST
 
-echo "@$1"
-/usr/bin/btrfs subvolume delete -c /root/topsubs/@$1
+    /usr/bin/umount /home/"$1"
 
+    echo "@$1"
+    /usr/bin/btrfs subvolume delete -c /root/topsubs/@$1
 
-/usr/bin/btrfs subvolume snapshot $LATEST /root/topsubs/@$1
+    /usr/bin/btrfs subvolume snapshot $LATEST /root/topsubs/@$1
 
-/usr/bin/umount /dev/vda3
+    /usr/bin/umount /dev/vda3
 
-/usr/bin/mount /home/"$1"
+    /usr/bin/mount /home/"$1"
+}
 
+#Determine which function the user passed in to the command
 
-
+case "$1" in
+    "") ;;
+    rollback) "$@"; exit;;
+    *) echo "Unknown function: $1"; exit 2;;
+esac
